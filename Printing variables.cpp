b772f0bb915da3f2966988078fbe0a3131811771
Daniya Roghay <w.daniyaroghay@gmail.com>
2024-12-26 10:00:50 +0530

    Update Printing variables.cpp

--- Printing variables.cpp
+++ Printing variables.cpp
@@ -5,7 +5,13 @@ int main() {
     int a = 4, b = 5;
     float pi = 3.14;
     char  c = 'u';
+    bool isTrue = true;
     
-    cout <<"Here, the value of a is "<<a<<"\nThe value of b is "<<b<<"\nThe value of pi is "<<pi<<"\nThe value of c is "<<c;
+    cout <<"Here, the value of a is "<<a<<
+            "\nThe value of b is "<<b<<
+            "\nThe value of pi is "<<pi<<
+            "\nThe value of c is "<<c<<
+            "\nThe boolean gives us "<<isTrue;
+            
     return 0;
 }
