2eafea4fe9e3ba3a35dd03e158c942d1ec59bc02
Daniya Roghay <w.daniyaroghay@gmail.com>
2024-12-26 09:51:41 +0530

    Add files via upload

---
+++ Printing variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 4, b = 5;
+    float pi = 3.14;
+    char  c = 'u';
+    
+    cout <<"Here, the value of a is "<<a<<"\nThe value of b is "<<b<<"\nThe value of pi is "<<pi<<"\nThe value of c is "<<c;
+    return 0;
+}
