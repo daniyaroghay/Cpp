2dfe8667256f15e4b90f3602d89ec254d54c55d5
Daniya Roghay <w.daniyaroghay@gmail.com>
2025-01-01 07:14:10 +0530

    Add files via upload

---
+++ Arithmetic operators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 4, b = 2;
+    
+    cout << "The sum is: " << a+b << endl;
+    cout << "The difference is: " << a-b << endl;
+    cout << "The product is: " << a*b << endl;
+    cout << "The quotient is: " << a/b << endl;
+    cout << "The remainder is: " << a%b << endl;
+
+    return 0;
+}
---
+++ Inc dec ops.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 4, b = 2;
+    
+    cout << "The value of a++ is: " << a++ << endl;
+    cout << "The value of a-- is: " << a-- << endl;
+    cout << "The value of ++a is: " << ++a << endl;
+    cout << "The value of --a is: " << --a << endl;
+
+    return 0;
+}
